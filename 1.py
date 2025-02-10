5fb85382f6880b3be43ee1ad1aae3c876b7560c0
shoneaura <oreol1313@yandex.ru>
2025-02-10 20:47:53 +0300

    my first commit

---
+++ 1.py
@@ -0,0 +1,11 @@
+# Переключились на ветку «main»
+# Эта ветка соответствует «origin/main».
+# (venv) shoneaura@shoneaura:~/Work/Lessons/lesson 19/innodom$ git checkout 7
+# error: pathspec '7' did not match any file(s) known to git
+# (venv) shoneaura@shoneaura:~/Work/Lessons/lesson 19/innodom$ git branch
+#   1
+# * main
+# (venv) shoneaura@shoneaura:~/Work/Lessons/lesson 19/innodom$ git checkout 1
+# A       1.py
+# Переключились на ветку «1»git status
+
