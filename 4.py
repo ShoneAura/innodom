69adaabfb0db5509cf1cb013e2387f19c31f06c2
ShoneAura <96006964+ShoneAura@users.noreply.github.com>
2025-02-10 21:10:57 +0300

    Update 4.py

--- 4.py
+++ 4.py
@@ -9,7 +9,6 @@ class MeteoSerializer(serializers.ModelSerializer):
         model = Meteo
         fields = '__all__'
 
-class WeatherSerializer(SerializerSerializer):
-class WeatherSerializer(SerializerSerializer):
+class WeatherSerializer():
     start_date = serializers.DateTimeField()
     end_date = serializers.DateTimeField()
