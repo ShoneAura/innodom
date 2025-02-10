11c87978540e9d95cc6d93df8e79d24fce399080
ShoneAura <96006964+ShoneAura@users.noreply.github.com>
2025-02-10 21:01:05 +0300

    Update 4.py

---
+++ 4.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from rest_framework.serializers import Serializer
+
+from currency.models import Meteo
+
+
+class MeteoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Meteo
+        fields = '__all__'
+
+
+class WeatherSerializer(Serializer):
+    start_date = serializers.DateTimeField()
+    end_date = serializers.DateTimeField()
